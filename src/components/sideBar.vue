4079e7c2e7d9146c663539d73b08505465f8f3a3
周诚 <zhoucheng133@outlook.com>
2023-08-05 16:25:10 +0800

    完成侧边栏layout

--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -8,6 +8,19 @@
             <div class="changepButton">修改密码</div>
             <div class="logoutButton">注销</div>
         </div>
+
+        <div class="aboutButton"></div>
+
+        <div class="menu">
+            <div class="item" style="margin-top: 30px;"><i class="bi bi-vinyl icon"></i>专辑</div>
+            <div class="item"><i class="bi bi-mic icon"></i>艺人</div>
+            <div class="item"><i class="bi bi-bookmark icon"></i>歌曲流派</div>
+            <div class="item"><i class="bi bi-music-note-list icon"></i>所有歌曲</div>
+            <div class="item"><i class="bi bi-heart icon"></i>喜欢的歌曲</div>
+            <div class="divLine"></div>
+            <div class="listText">创建的歌单</div>
+            <div class="item"><i class="bi bi-list-ul icon"></i><div class="itemContainer">歌单123123123123</div></div>
+        </div>
     </div>
 </template>
 
@@ -31,6 +44,56 @@ export default {
 </script>
 
 <style scoped>
+.itemContainer{
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.listText{
+    user-select: none;
+    width: 180px;
+    padding: 0px 10px 10px 20px;
+    display: flex;
+    justify-content: flex-start;
+    color: grey;
+}
+.divLine{
+    margin-top: 5px;
+    margin-bottom: 5px;
+    border: 1px solid lightgrey;
+    background-color: lightgrey;
+    width: 80%;
+}
+.menu{
+    margin-top: 30px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: calc(100vh - 160px);
+    overflow: scroll;
+}
+.icon{
+    font-size: 16px;
+    margin-right: 8px;
+}
+.item:hover{
+    background-color: rgb(225, 225, 225);
+    cursor: pointer;
+}
+.item{
+    display: flex;
+    font-size: 16px;
+    width: 180px;
+    padding: 10px 10px 10px 20px;
+    border-radius: 10px;
+    user-select: none;
+    transition: all ease-in-out .3s;
+    text-align: left;
+    white-space: nowrap;
+    justify-content: flex-start;
+    text-overflow: ellipsis;
+}
 .changepButton:hover{
     color: #1890ff;
 }
@@ -50,7 +113,7 @@ export default {
 .userOp{
     display: flex;
     position: absolute;
-    bottom: 20px;
+    bottom: 50px;
     left: 50%;
     transform: translateX(-50%);
 }
@@ -62,14 +125,14 @@ export default {
     text-overflow: ellipsis;
 }
 .userinfo{
-    width: 160px;
+    max-width: 180px;
     padding: 10px 10px 10px 10px;
     border-radius: 10px;
     font-size: 20px;
     user-select: none;
     display: flex;
     position: absolute;
-    bottom: 40px;
+    bottom: 70px;
     left: 50%;
     transform: translateX(-50%);
     align-items: center;
@@ -81,5 +144,7 @@ export default {
     width: 200px;
     height: 100vh;
     background-color: rgb(242, 242, 242);
+    align-items: center;
+    overflow: scroll;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import axios from 'axios'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 Vue.config.productionTip = false
 
