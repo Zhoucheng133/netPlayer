b23721ec8f9a1ec6a74369bcb44488fd1537665d
周诚 <zhoucheng133@outlook.com>
2023-08-05 23:24:25 +0800

    完成关于界面

--- src/components/aboutView.vue
+++ src/components/aboutView.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        {{ version }}
+    <div class="bg">
+        <div class="container">
+            <img src="@/assets/icon.png" alt="" width="200px">
+            <div class="name">netPlayer</div>
+            <div class="version">v{{ version }}&nbsp;Dev</div>
+            <div class="copyright">Developed by zhouc</div>
+            <div class="opensource" @click="showLicenses">About the open source licenses</div>
+        </div>
     </div>
 </template>
 
@@ -12,7 +18,20 @@ export default {
         }
     },
     methods: {
-        
+        showLicenses(){
+            const h = this.$createElement;
+            this.$info({
+                title: 'Open source licenses',
+                content: h('div', {}, [
+                    h('div', 'Vue.js (MIT License)'),
+                    h('div', 'Ant Design Vue (MIT License)'),
+                    h('div', 'axios (MIT License)'),
+                    h('div', 'crypto-js (MIT License)'),
+                ]),
+                okText: "完成",
+                onOk() { },
+            });
+        }
     },
     mounted() {
         
@@ -24,5 +43,33 @@ export default {
 </script>
 
 <style scoped>
-
+.opensource:hover{
+    cursor: pointer;
+    color: #1890ff;
+}
+.opensource{
+    color: rgb(170, 170, 170);
+    transition: all ease-in-out .3s;
+}
+.copyright{
+    margin-top: 30px;
+}
+.name{
+    font-size: 28px;
+    font-weight: bold;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.bg{
+    user-select: none;
+    display: flex;
+    height: calc(100vh - 30px);
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+}
 </style>
