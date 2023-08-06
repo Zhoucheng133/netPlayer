a5c2a8d539344aaf7e640e021f4bc13222eae0c5
周诚 <zhoucheng133@outlook.com>
2023-08-06 17:31:26 +0800

    完成歌曲统计

--- src/components/listView.vue
+++ src/components/listView.vue
@@ -2,7 +2,7 @@
     <div>
     <a-page-header
         :title="shownTitle"
-        sub-title="This is a subtitle" />
+        :sub-title="subTitle" />
     </div>
 </template>
 
@@ -15,6 +15,7 @@ export default {
     data() {
         return {
             shownTitle: "",
+            subTitle: "",
         }
     },
     methods: {
@@ -22,21 +23,26 @@ export default {
             switch (this.nowPage) {
                 case 'allSongs':
                     this.shownTitle="所有歌曲";
+                    this.subTitle="";
                     break;
                 case 'albums':
                     this.shownTitle="专辑";
+                    this.subTitle="";
                     break;
                 case 'artists':
                     this.shownTitle="艺人";
+                    this.subTitle="";
                     break;
                 case 'songStyles':
                     this.shownTitle="歌曲流派";
+                    this.subTitle="";
                     break;
                 case 'lovedSongs':
                     this.shownTitle="喜欢的歌曲";
                     break;
                 case 'playList':
                     this.shownTitle=this.playList.name;
+                    this.subTitle="合计"+this.playList.songCount+"首歌";
                     break;
             }
         }
