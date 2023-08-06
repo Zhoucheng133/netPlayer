e776015579006e488bbfec47989a7caa611a1f77
周诚 <zhoucheng133@outlook.com>
2023-08-06 17:54:49 +0800

    完成播放列表基本布局

--- src/components/listView.vue
+++ src/components/listView.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
+    <div class="bg">
         <a-page-header
             :title="shownTitle"
             :sub-title="subTitle" />
-        
-        <div></div>
-        
+
+        <div v-if="nowPage!='albums' && nowPage!='artists' && nowPage!='songStyles'">
+            <div class="container_fix">
+                <div class="item">序号</div>
+                <div class="item">歌曲名</div>
+                <div class="item">歌手</div>
+                <div class="item">时长</div>
+                <div class="item">
+                    <svg width="15" height="15" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 8C8.92487 8 4 12.9249 4 19C4 30 17 40 24 42.3262C31 40 44 30 44 19C44 12.9249 39.0751 8 33 8C29.2797 8 25.9907 9.8469 24 12.6738C22.0093 9.8469 18.7203 8 15 8Z" fill="none" stroke="#333" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                </div>
+            </div>
+
+            <div class="mainArea">
+
+            </div>
+        </div>
     </div>
 </template>
 
@@ -68,5 +81,38 @@ export default {
 </script>
 
 <style scoped>
-
+.mainArea{
+    position: fixed;
+    margin-top: 50px;
+    width: calc(100% - 248px);
+    height: calc(100% - 64px - 60px - 100px);
+    background-color: lightcyan;
+    margin-left: 24px;
+}
+.container{
+    margin-left: 24px;
+    display: grid;
+    grid-template-columns: 50px auto 100px 50px 50px;
+    width: 100%;
+    height: 50px;
+}
+.item{
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    font-size: 15px;
+}
+.bg{
+    user-select: none;
+    height: calc(100vh - 30px) !important;
+}
+.container_fix{
+    position: fixed;
+    margin-left: 24px;
+    display: grid;
+    grid-template-columns: 50px auto 100px 50px 50px;
+    width: calc(100% - 248px);
+    background-color: rgb(242, 242, 242);
+    height: 50px;
+}
 </style>
