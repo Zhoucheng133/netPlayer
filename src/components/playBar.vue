1f394dbca21ee23a8910c8da2de93446c89659e6
周诚 <zhoucheng133@outlook.com>
2023-08-06 19:22:45 +0800

    改进播放条

--- src/components/playBar.vue
+++ src/components/playBar.vue
@@ -26,8 +26,12 @@ export default {
 <style scoped>
 .bar{
     height: 100px;
-    background-color: rgb(242, 242, 242);
+    background-color: rgba(255,255,255,0.3);
+    backdrop-filter: blur(15px);
     /* width: 100vw; */
-    width: calc(100vw - 224px - 24px);
+    width: calc(100vw - 240px - 40px);
+    border-radius: 10px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    /* border: 1px solid; */
 }
 </style>
