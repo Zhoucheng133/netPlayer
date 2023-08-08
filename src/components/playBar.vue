43915bd6e0a3fd5f495807168df4c74c3bdd9de9
周诚 <zhoucheng133@outlook.com>
2023-08-09 02:18:43 +0800

    改进界面

--- src/components/playBar.vue
+++ src/components/playBar.vue
@@ -231,7 +231,7 @@ export default {
     /* width: 100vw; */
     width: calc(100vw - 240px - 40px);
     border-radius: 10px;
-    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.3);
     /* border: 1px solid; */
     overflow: hidden;
 }
