9df5a9b514312c5f596b7638dce9dabd469bacb8
周诚 <zhoucheng133@outlook.com>
2023-08-14 17:28:12 +0800

    完成搜索单曲功能

--- src/components/searchView.vue
+++ src/components/searchView.vue
@@ -1,29 +1,107 @@
 <template>
     <div>
-        <a-input-search @search="onSearch" enter-button></a-input-search>
+        <a-input-search v-model="inputValue" @search="onSearch" enter-button></a-input-search>
         <a-tabs default-active-key="song" @change="callback">
             <a-tab-pane key="song" tab="单曲">
-                Content of Tab Pane 1
+                <div v-if="shownList.length==0">对关键字进行搜索</div>
+                <div v-else>
+                    <div class="container_fix">
+                        <div class="item">序号</div>
+                        <div class="item">歌曲名</div>
+                        <div class="item">歌手</div>
+                        <div class="item">时长</div>
+                    </div>
+                    <div class="mainArea">
+                        <div v-for="(item, index) in shownList.song" :key="index" class="container">
+                            <div class="item"><div class="itemContent">{{ index+1 }}</div></div>
+                            <div class="item"><div class="itemContent">{{ item.title }}</div></div>
+                            <div class="item"><div class="itemContent">{{ item.artist }}</div></div>
+                            <div class="item"><div class="itemContent">{{ getSongTime(item.duration) }}</div></div>
+                        </div>
+                    </div>
+                </div>
             </a-tab-pane>
             <a-tab-pane key="artist" tab="艺人">
-                Content of Tab Pane 2
+                <div v-if="shownList.length==0">对关键字进行搜索</div>
             </a-tab-pane>
             <a-tab-pane key="album" tab="专辑">
-                Content of Tab Pane 3
+                <div v-if="shownList.length==0">对关键字进行搜索</div>
             </a-tab-pane>
         </a-tabs>
     </div>
 </template>
 
 <script>
+import { ipcRenderer } from 'electron';
 export default {
+    beforeDestroy() {
+        ipcRenderer.removeAllListeners('searchResult');
+    },
     data() {
         return {
-            
+            shownList: [],
+            inputValue: ""
         }
+    },
+    methods: {
+        getSongTime(duration){
+            var min=parseInt(duration/60);
+            var sec=duration%60;
+            return sec<10?min+":0"+sec:min+":"+sec
+        },
+        onSearch(){
+            if(this.inputValue==''){
+                this.shownList=[];
+            }else{
+                ipcRenderer.send('searchRequest', localStorage.getItem("url"), localStorage.getItem("username"), localStorage.getItem("salt"), localStorage.getItem("token"), this.inputValue);
+            }
+        },
+        callback(){
+
+        },
+        searchResult(event, resp){
+            this.shownList=resp.searchResult2
+        }
+    },
+    mounted() {
+        ipcRenderer.removeAllListeners('searchResult');
+        ipcRenderer.on('searchResult', this.searchResult);
     },
 }
 </script>
 
 <style scoped>
+.mainArea > .container:hover{
+    background-color: rgb(220, 220, 220);
+}
+.mainArea{
+    overflow-y: overlay;
+    overflow-x: hidden;
+    position: fixed;
+    margin-top: 50px;
+    width: 100%;
+    height: calc(100vh - 30px - 64px - 50px);
+    padding-bottom: 220px;
+}
+.item{
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+    font-size: 14px;
+}
+.container{
+    display: grid;
+    grid-template-columns: 50px calc(100vw - 200px - 48px - 50px - 150px - 70px) 150px 70px;
+    width: 100%;
+    height: 50px;
+    transition: all ease-in-out .2s;
+}
+.container_fix{
+    position: fixed;
+    display: grid;
+    grid-template-columns: 50px auto 150px 70px;
+    width: 100%;
+    background-color: rgb(242, 242, 242);
+    height: 50px;
+}
 </style>
