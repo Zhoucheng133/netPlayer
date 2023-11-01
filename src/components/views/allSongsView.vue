ca3909622ae74d4b1f5f39688a4d65fbe44acc6e
zhoucheng <zhoucheng133@outlook.com>
2023-11-01 16:14:05 +0800

    完成所有歌曲title重制

--- src/components/views/allSongsView.vue
+++ src/components/views/allSongsView.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
-    ALLSONGS
+    <a-page-header 
+      title="所有歌曲"
+			:sub-title="subTitle">
+			<template slot="extra">
+				<div class="toolBar">
+					<a-input-search placeholder="输入搜索内容" style="width: 200px;margin-right: 20px;"
+						@search="onSearch" allowClear v-model="inputSearch" @change="onSearch" />
+					<a-button icon="redo" type="primary" shape="round" @click="reloadList"></a-button>
+				</div>
+			</template>
+		</a-page-header>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      subTitle: "",
+      inputSearch: ""
+    }
+  },
+  methods: {
+    onSearch() {
+			if (this.inputSearch == "") {
+				this.isSearch = false;
+			}
+			this.isSearch = true;
+			this.searchList = this.filterArrayByString(this.shownList, this.inputSearch);
+		},
+    reloadList(){
+      // TODO 重载歌曲
+    }
+  },
 }
 </script>
