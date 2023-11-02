79369136b3695a9d873f8b41f651609b280510b2
zhoucheng <zhoucheng133@outlook.com>
2023-11-02 21:58:21 +0800

    添加播放列表的header

--- src/components/views/playListView.vue
+++ src/components/views/playListView.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
-    PLAYLIST
+    <a-page-header 
+      :title=playList.name
+			:sub-title="subTitle">
+			<template slot="extra">
+				<div class="toolBar">
+					<a-input-search placeholder="输入搜索内容" style="width: 200px;margin-right: 20px;"
+						@search="onSearch" allowClear v-model="inputSearch" @change="onSearch" />
+					<a-button icon="redo" type="primary" shape="round" @click="reloadList"></a-button>
+				</div>
+			</template>
+		</a-page-header>
   </div>
 </template>
 
 <script>
 export default {
-  
+  props: {
+    playList: Object
+  },
+  data() {
+    return {
+      inputSearch: "",
+      subTitle: ""
+    }
+  },
+  methods: {
+    change(){},
+    onSearch(){},
+    reloadList(){},
+  },
 }
 </script>
