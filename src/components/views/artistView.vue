db0fb060cc79a42c9b9c791c886a29f7e797f606
zhoucheng <zhoucheng133@outlook.com>
2023-11-04 19:30:01 +0800

    显示艺人列表

--- src/components/views/artistView.vue
+++ src/components/views/artistView.vue
@@ -11,6 +11,25 @@
 				</div>
 			</template>
 		</a-page-header>
+
+    <div class="container_fix_artist">
+      <div class="item">序号</div>
+      <div class="item">艺人</div>
+      <div class="item">专辑数量</div>
+    </div>
+    <div class="mainArea" v-if="!isSearch">
+      <div class="container_artist" v-for="(item, index) in shownList" :key="index" @dblclick="showArtistContent(item)">
+        <div class="item">
+          <div class="itemContent">{{ index + 1 }}</div>
+        </div>
+        <div class="item">
+          <div class="itemContent">{{ item.name }}</div>
+        </div>
+        <div class="item">
+          <div class="itemContent">{{ item.albumCount }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,11 +44,19 @@ export default {
       inputSearch: "",
       subTitle: "",
       shownList: [],
+      isSearch: false,
     }
   },
   methods: {
-    onSearch(){},
-    reloadList(){},
+    showArtistContent(){
+      // TODO 艺人信息
+    },
+    onSearch(){
+      // TODO 搜索
+    },
+    reloadList(){
+      // TODO 刷新列表
+    },
     loadList(){
       axios.get(this.userInfo.url+"/rest/getIndexes?v=1.13.0&c=netPlayer&f=json&u="+this.userInfo.username+"&s="+this.userInfo.salt+"&t="+this.userInfo.token)
       .then((response)=>{
@@ -54,3 +81,51 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.mainArea {
+	overflow-y: overlay;
+	overflow-x: hidden;
+	position: fixed;
+	margin-top: 50px;
+	width: calc(100% - 248px);
+	height: calc(100vh - 30px - 64px - 50px);
+	margin-left: 24px;
+	padding-bottom: 130px;
+}
+
+.container_artist {
+	display: grid;
+	grid-template-columns: 50px calc(100vw - 200px - 48px - 50px - 100px) 100px;
+	width: 100%;
+	height: 50px;
+	transition: all ease-in-out .2s;
+}
+
+.mainArea>.container_artist:hover {
+	background-color: rgb(240, 240, 240);
+}
+.itemContent {
+	width: 100%;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	text-align: left;
+	white-space: nowrap;
+}
+.item {
+	display: flex;
+	align-items: center;
+	padding-left: 10px;
+	font-size: 14px;
+}
+
+.container_fix_artist {
+	position: fixed;
+	margin-left: 24px;
+	display: grid;
+	grid-template-columns: 50px auto 100px;
+	width: calc(100% - 248px);
+	background-color: rgb(242, 242, 242);
+	height: 50px;
+}
+</style>
