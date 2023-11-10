adedb64106ad7ebb9e5de04489929c14f385a53c
zhoucheng <zhoucheng133@outlook.com>
2023-11-10 12:15:07 +0800

    格式化代码

--- src/background.js
+++ src/background.js
@@ -42,15 +42,15 @@ function createMenu() {
 			label: 'File',
 			submenu: [
 				{
-					label:"关于 netPlayer",
+					label: "关于 netPlayer",
 					click: () => {
 						win.webContents.send('toAbout');
 					},
 				},
 				{ type: 'separator' },
 				{
-					label:"隐藏 netPlayer",
-					role:"hide",
+					label: "隐藏 netPlayer",
+					role: "hide",
 					accelerator: "CmdOrCtrl+H",
 				},
 				{
@@ -116,12 +116,12 @@ function createMenu() {
 			label: "窗口",
 			submenu: [
 				{
-					label:"最小化",
-					role:"minimize"
+					label: "最小化",
+					role: "minimize"
 				},
 				{
-					label:"缩放",
-					role:"zoom",
+					label: "缩放",
+					role: "zoom",
 				}
 			]
 		}
@@ -155,57 +155,57 @@ function registerMediaKeyShortcuts() {
 
 // 新建歌单
 ipcMain.on("newListRequest", async (event, url, username, salt, token, listName) => {
-	var resp=undefined;
-	await axios.post(url+"/rest/createPlaylist?v=1.13.0&c=netPlayer&f=json&u="+username+"&s="+salt+"&t="+token+"&name="+listName)
-	.then((response)=>{
-		resp=response.data['subsonic-response'];
-	})
-	.catch(()=>{
-		resp=null;
-	})
+	var resp = undefined;
+	await axios.post(url + "/rest/createPlaylist?v=1.13.0&c=netPlayer&f=json&u=" + username + "&s=" + salt + "&t=" + token + "&name=" + listName)
+		.then((response) => {
+			resp = response.data['subsonic-response'];
+		})
+		.catch(() => {
+			resp = null;
+		})
 
 	event.reply('newListResult', resp);
 });
 
 // 重命名歌单
 ipcMain.on("renameListRequest", async (event, url, username, salt, token, listID, listName) => {
-	var resp=undefined;
-	await axios.post(url+"/rest/updatePlaylist?v=1.13.0&c=netPlayer&f=json&u="+username+"&s="+salt+"&t="+token+"&playlistId="+listID+"&name="+listName)
-	.then((response)=>{
-		resp=response.data['subsonic-response'];
-	})
-	.catch(()=>{
-		resp=null;
-	})
+	var resp = undefined;
+	await axios.post(url + "/rest/updatePlaylist?v=1.13.0&c=netPlayer&f=json&u=" + username + "&s=" + salt + "&t=" + token + "&playlistId=" + listID + "&name=" + listName)
+		.then((response) => {
+			resp = response.data['subsonic-response'];
+		})
+		.catch(() => {
+			resp = null;
+		})
 
 	event.reply('renameListResult', resp);
 });
 
 // 删除歌单
 ipcMain.on("delListRequest", async (event, url, username, salt, token, listID) => {
-	var resp=undefined;
-	await axios.post(url+"/rest/deletePlaylist?v=1.13.0&c=netPlayer&f=json&u="+username+"&s="+salt+"&t="+token+"&id="+listID)
-	.then((response)=>{
-		resp=response.data['subsonic-response'];
-	})
-	.catch(()=>{
-		resp=null;
-	})
+	var resp = undefined;
+	await axios.post(url + "/rest/deletePlaylist?v=1.13.0&c=netPlayer&f=json&u=" + username + "&s=" + salt + "&t=" + token + "&id=" + listID)
+		.then((response) => {
+			resp = response.data['subsonic-response'];
+		})
+		.catch(() => {
+			resp = null;
+		})
 
 	event.reply('delListResult', resp);
 });
 
 // 加载歌单
 ipcMain.on("listRequest", async (event, url, username, salt, token, id) => {
-	var resp=undefined;
+	var resp = undefined;
 
-	await axios.post(url+"/rest/getPlaylist?v=1.13.0&c=netPlayer&f=json&u="+username+"&s="+salt+"&t="+token+"&id="+id)
-	.then((response)=>{
-		resp=response.data['subsonic-response'];
-	})
-	.catch(()=>{
-		resp=null;
-	})
+	await axios.post(url + "/rest/getPlaylist?v=1.13.0&c=netPlayer&f=json&u=" + username + "&s=" + salt + "&t=" + token + "&id=" + id)
+		.then((response) => {
+			resp = response.data['subsonic-response'];
+		})
+		.catch(() => {
+			resp = null;
+		})
 
 	event.reply('listResult', resp);
 });
@@ -213,47 +213,47 @@ ipcMain.on("listRequest", async (event, url, username, salt, token, id) => {
 
 // 加载所有的歌单
 ipcMain.on("playlistRequest", async (event, url, username, salt, token) => {
-	var resp=undefined;
+	var resp = undefined;
 
-	await axios.post(url+"/rest/getPlaylists?v=1.13.0&c=netPlayer&f=json&u="+username+"&s="+salt+"&t="+token)
-	.then((response)=>{
-		resp=response.data['subsonic-response'];
-	})
-	.catch(()=>{
-		resp=null;
-	})
+	await axios.post(url + "/rest/getPlaylists?v=1.13.0&c=netPlayer&f=json&u=" + username + "&s=" + salt + "&t=" + token)
+		.then((response) => {
+			resp = response.data['subsonic-response'];
+		})
+		.catch(() => {
+			resp = null;
+		})
 
 	event.reply('playlistResult', resp, salt, token);
 });
 
 // 获取系统信息
-ipcMain.on("getSysRequest",async(event)=>{
-	if(process.platform == 'darwin'){
+ipcMain.on("getSysRequest", async (event) => {
+	if (process.platform == 'darwin') {
 		event.reply('getSysResult', 'macOS');
-	}else if(process.platform=='win32' ){
+	} else if (process.platform == 'win32') {
 		event.reply('getSysResult', 'Windows');
-	}else{
+	} else {
 		event.reply('getSysResult', 'Linux');
 	}
 });
 
 // 最小化窗口函数
-ipcMain.on("winMin",async(event)=>{
+ipcMain.on("winMin", async (event) => {
 	win.minimize();
 }),
 
-// 关闭窗口函数
-ipcMain.on("winClose",async(event)=>{
-	app.exit();
-})
+	// 关闭窗口函数
+	ipcMain.on("winClose", async (event) => {
+		app.exit();
+	})
 
 // 最大化窗口
-ipcMain.on("winMax",async(event)=>{
+ipcMain.on("winMax", async (event) => {
 	win.maximize();
 })
 
 // 还原窗口
-ipcMain.on("winRestore",async(event)=>{
+ipcMain.on("winRestore", async (event) => {
 	win.restore();
 })
 
